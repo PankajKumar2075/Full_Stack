c4878f1c2b993ae694ef81e957ebcb2ee1a65b37
PankajKumar2075 <pankajnehra646@gmail.com>
2026-06-23 20:24:16 +0530

    Initial commit

---
+++ Backend/src/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+    cors({
+        origin: "*",
+        credentials: true
+    })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+
+app.use("/api/v1/healthcheck", healthCheckRouter);
+
+export default app;
