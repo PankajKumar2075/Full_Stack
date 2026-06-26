ffc55f980910c437dd899527eacd1bb42923eeb5
PankajKumar2075 <pankajnehra646@gmail.com>
2026-06-26 12:59:06 +0530

    Add delete comment API

--- Backend/src/controllers/comment.controller.js
+++ Backend/src/controllers/comment.controller.js
@@ -40,6 +40,72 @@ const addComment = asyncHandler(async (req, res) => {
     );
 });
 
+const getComments = asyncHandler(async (req, res) => {
+
+    const { postId } = req.params;
+
+    const post = await Post.findById(postId);
+
+    if (!post) {
+        throw new ApiError(
+            404,
+            "Post not found"
+        );
+    }
+
+    const comments = await Comment.find({
+        post: postId
+    })
+    .populate(
+        "owner",
+        "fullName username avatar"
+    )
+    .sort({
+        createdAt: -1
+    });
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            comments,
+            "Comments fetched successfully"
+        )
+    );
+});
+
+const deleteComment = asyncHandler(async (req, res) => {
+
+    const { commentId } = req.params;
+
+    const comment = await Comment.findById(commentId);
+
+    if (!comment) {
+        throw new ApiError(
+            404,
+            "Comment not found"
+        );
+    }
+
+    if (comment.owner.toString() !== req.user._id.toString()) {
+        throw new ApiError(
+            403,
+            "You are not authorized to delete this comment"
+        );
+    }
+
+    await Comment.findByIdAndDelete(commentId);
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            {},
+            "Comment deleted successfully"
+        )
+    );
+});
+
 export {
-    addComment
+    addComment,
+    getComments,
+    deleteComment
 };
--- Backend/src/routes/comment.route.js
+++ Backend/src/routes/comment.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { addComment } from "../controllers/comment.controller.js";
+import { addComment,getComments,deleteComment } from "../controllers/comment.controller.js";
 
 const router = Router();
 
@@ -8,6 +8,19 @@ router.post(
     "/:postId",
     verifyJWT,
     addComment
+);
+
+router.get(
+    "/:postId",
+    verifyJWT,
+    getComments
+);
+
+
+router.delete(
+    "/:commentId",
+    verifyJWT,
+    deleteComment
 );
 
 export default router;
