20ce5639a0135d5edd1dbfbc0c569b0d79fcbe37
PankajKumar2075 <pankajnehra646@gmail.com>
2026-06-30 12:47:55 +0530

    Setup authentication context and auth services

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -1,3 +1,41 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import api from "../api/axios";
 
 export const AuthContext = createContext();
+
+function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+
+  const [loading, setLoading] = useState(true);
+
+  const fetchCurrentUser = async () => {
+    try {
+      const res = await api.get("/users/current-user");
+
+      setUser(res.data.data);
+    } catch (error) {
+      setUser(null);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCurrentUser();
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        setUser,
+        loading,
+        fetchCurrentUser,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthProvider;
--- Frontend/src/services/auth.service.js
+++ Frontend/src/services/auth.service.js
@@ -1,1 +1,19 @@
-export {};
+import api from "../api/axios";
+
+export const registerUser = async (formData) => {
+  const response = await api.post("/users/register", formData);
+
+  return response.data;
+};
+
+export const loginUser = async (data) => {
+  const response = await api.post("/users/login", data);
+
+  return response.data;
+};
+
+export const logoutUser = async () => {
+  const response = await api.post("/users/logout");
+
+  return response.data;
+};
